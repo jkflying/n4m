43dc02581c2784ea32cca6bc759d5ac0d7907d9f
Julian Kent <jkflying@gmail.com>
2026-03-14 21:09:37 +0100

    Fix ci

--- tools/install_dependencies.sh
+++ tools/install_dependencies.sh
@@ -1,8 +1,8 @@
 #! /bin/bash
 
 set -ex
-sudo apt-get update || (apt-get update && apt-get install sudo)
-sudo apt-get install -y cmake ccache ninja-build libopencv-dev libgtest-dev libspdlog-dev clang-format-14 clang-tidy-18 clang-18 build-essential git lcov libonnxruntime-dev
+sudo apt-get update || (apt-get update && apt-get install -y sudo)
+sudo apt-get install -y cmake ccache ninja-build libopencv-dev libgtest-dev libspdlog-dev clang-format-14 clang-tidy-18 clang-18 build-essential git lcov libonnxruntime-dev python3-venv
 
 cd "$(dirname "$0")/.."
 
@@ -10,4 +10,5 @@ rm -rf external/venv/
 python3 -m venv external/venv
 external/venv/bin/pip install fastcov
 
+git config --global --add safe.directory "$(pwd)"
 git submodule update --init --recursive
